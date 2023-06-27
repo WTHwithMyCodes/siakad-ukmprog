81908a4e64e30840ee81e4f57581a7d863ec4459
WTHwithMyCodes <kertomulyo01@gmail.com>
2023-06-27 15:20:16 +0700

    Video 2 Siakad 26:15

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,1 +1,10 @@
-<h1>Ini halaman Home cuy</h1>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="text-center">
+            <h1><b>Sistem Informasi Akademik Kampus</b></h1>
+        </div>
+    </div>
+    <div class="col-sm-12">
+            <img class="img-responsive pad" src="<?= base_url('gambar/academic.jpg') ?>" alt="Photo">
+    </div>      
+</div>
